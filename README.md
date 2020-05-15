f99efeabeef4bf292a50be9dd875fb8285011beb
LenoirRemi <biaremi@hotmail.fr>
2020-05-15 09:31:52 +0200

    emulation

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# Dev_App_Comp_2020
+# POC Anonymisation des données
 
+## Introduction
+
+Le but de ce [POC](https://fr.wikipedia.org/wiki/Preuve_de_concept) est de prouver que l'on peut manipuler des informations permettant l'identification des contacts inter-appareils, en n'utilisant aucune information concernant le téléphone et donc implicitement la personne qui le détient.
+
+## Protocole
+
+À l'installation de l'application un identifiant unique est généré, il s'agit de la clé secrète. ([UUID](https://fr.wikipedia.org/wiki/Universal_Unique_Identifier))
+
+![Installation](gitimage/Installation.png)
+
+À intervalles réguliers, l'application incrémente un compteur `i` et l'utilise dans une fonction de hachage pour générer un identifiant temporaire.
+
+![Intervalles](gitimage/Intervalles.png)
+
+Par exemple :
+```dart
+cle = "14e673b3-30cf-467f-b07d-7d67a888ed01"
+i = 10
+ID_temporaire = hacher(i, cle)
+// ID_temporaire vaut 1e973a64e2be2c3f89da1c12e080ab0322ccaa2fbce2a8fe07f40ed4de45868b7711af66cdeecefce81cd7751a6373757ae6258802a39dac3ddadd7ae6f0ba34
+```
+
+Lorsque des appareils rentrent en contacts, ils s'échangent leur identifiant temporaire et leur compteur `i`. Les données reçues sont sauvegardés dans l'appareil.
+
+![Echanges](gitimage/Echanges.png)
+
+Si un utilisateur est diagnostiqué porteur d'un virus, il peut volontairement signaler son état. Pour cela l'application met à disposition un bouton qui engendrera les interactions suivantes :
+1. L'application envoie la clé au serveur.
+2. Le serveur propage la clé sur tous les appareils.
+3. Chaque appareil compare la clé avec les identifiants temporaires qu'il a échangé.
+	1. L'application va hacher la clé reçue par le `i` de chaque informations échangées.
+	2. Si le résultat du hachage correspond à l'identifiant échangé alors il y a eu un contact.
+4. L'appareil signal au cas échéant le risque de contamination.
+
+![Signal](gitimage/Signal.png)
+
+Exemple, le téléphone `B` à échangé ces informations :
+
+|  i   |                        ID_temporaire                         |
+| :--: | :----------------------------------------------------------: |
+| 655  | 73a0076bc8fecb07f7b191499c775c298afe2db55d4b53209d996a8e9d8177... |
+|  10  | 1e973a64e2be2c3f89da1c12e080ab0322ccaa2fbce2a8fe07f40ed4de45868... |
+|  22  | e6be5cbb9d84a45c140a6e9971f8be4e5547240765978055016df00f109e2d... |
+|  10  | a27a409da2214f0a56b6600a99bbc6e26f06c011b6cff56b81634558d200a35... |
+
+À la réception de la clé `14e673b3-30cf-467f-b07d-7d67a888ed01`, il va exécuter l'algorithme décris précédemment ce qui donne : 
+
+(`résultat = hacher(i, clé)`)
+
+|  i   |               ID_temporaire               |                 résultat                  |
+| :--: | :---------------------------------------: | :---------------------------------------: |
+| 655  |  73a0076bc8fecb07f7b191499c775c298af...   |   ea70e2bf80c804320c5d34dba1f2a85360...   |
+|  10  | **1e973a64e2be2c3f89da1c12e080ab0322...** | **1e973a64e2be2c3f89da1c12e080ab0322...** |
+|  22  |   e6be5cbb9d84a45c140a6e9971f8be4e55...   |   327b817a30758c2be5a91f4dbac0b6134e...   |
+|  10  |   a27a409da2214f0a56b6600a99bbc6e26f...   |   1e973a64e2be2c3f89da1c12e080ab0322...   |
+
+Ici la deuxième ligne a un résultat correspondant à l'identifiant temporaire, il y a donc eu un contact entre le téléphone `B` et un porteur du virus.
+
+## Code
+
+## Conclusion / Analyse
+
+Les informations qui transitent ne sont que celles des personnes infectés qui sont volontaire de partager leur clé.
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,6 +12,8 @@ class _HomeState extends State<Home> {
   Future<String> _uuid;
   Future<String> _i;
   Future<String> _tempID;
+  bool _emulate = false;
+  bool _risk = false;
 
   @override
   void initState() {
@@ -20,7 +22,7 @@ class _HomeState extends State<Home> {
   }
 
   Future<void> init() async {
-    await deleteAll();
+    // await deleteAll();
     await manageUUID();
     setState(() {
       _uuid = getUUID();
@@ -37,15 +39,23 @@ class _HomeState extends State<Home> {
     });
   }
 
+  emulateReception() {
+    setState(() {
+      _emulate = true;
+      _risk = compareAllHash();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("CovidAPP"),
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               FutureBuilder(
                 future: _uuid,
@@ -97,8 +107,126 @@ class _HomeState extends State<Home> {
                 onPressed: () => updateTempID(),
                 child: Text("Generer ID temporaire"),
               ),
+              Divider(),
+              RaisedButton(
+                onPressed: () => emulateReception(),
+                child: Text("Emuler la reception de clés"),
+              ),
+              Divider(),
+              Center(child: _emulate ? TableCle() : Container()),
+              _emulate ? Divider() : Container(),
+              Center(child: _emulate ? TableCompare() : Container()),
+              _emulate ? Divider() : Container(),
+              _emulate
+                  ? Center(
+                      child: _risk
+                          ? Text(
+                              "Le téléphone a été en contacte avec une/des personne contaminée.",
+                              style: TextStyle(fontSize: 20.0))
+                          : Text(
+                              "Le téléphone n'a pas été en contacte avec une/des personne contaminée.",
+                              style: TextStyle(fontSize: 20.0)))
+                  : Container(),
+              SizedBox(
+                height: 50.0,
+              )
             ],
           ),
         ));
+  }
+}
+
+class TableCle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<TableRow> rows = <TableRow>[
+      new TableRow(children: <Widget>[
+        Text(
+          "Clés reçues du serveur",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+        )
+      ])
+    ];
+
+    for (String cle in EMULATED_BACKEND_LIST) {
+      rows.add(new TableRow(
+        children: [
+          Text(
+            cle,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16.0),
+          )
+        ],
+      ));
+    }
+
+    return Table(border: TableBorder.all(color: Colors.black), children: rows);
+  }
+}
+
+class TableCompare extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<TableRow> rows = <TableRow>[
+      new TableRow(children: <Widget>[
+        Text(
+          "i reçus",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16.0),
+        ),
+        Text(
+          "ID reçus",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16.0),
+        ),
+        Text(
+          "ID calculés",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16.0),
+        )
+      ])
+    ];
+
+    var meet = false;
+    var res = "";
+    for (final y in EMULATED_BACKEND_LIST) {
+      for (final z in EMULATED_BLUETOOTH_MEETINGS) {
+        meet = false;
+        res = generateTempID(z["i"].toString(), y);
+        if (z["hash"] == res) {
+          meet = true;
+        }
+        rows.add(new TableRow(
+          children: [
+            Text(
+              z["i"].toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: meet ? FontWeight.bold : FontWeight.normal),
+            ),
+            Text(
+              z["hash"].toString().substring(0, 12) + "...",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: meet ? FontWeight.bold : FontWeight.normal),
+            ),
+            Text(
+              res.toString().substring(0, 12) + "...",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: meet ? FontWeight.bold : FontWeight.normal),
+            ),
+          ],
+        ));
+      }
+      rows.add(new TableRow(
+        children: [Text(""), Container(), Container()],
+      ));
+    }
+    rows.removeLast();
+    return Table(border: TableBorder.all(color: Colors.black), children: rows);
   }
 }
